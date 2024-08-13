00e184dc808ae6603f75f32bfc7cdfd7a033b6ef
princechaudhary <dushyantchaudhary512@gmail.com>
2024-08-13 11:48:37 +0530

    Prince

---
+++ script.js
@@ -0,0 +1,53 @@
+// let a = 10;
+// document.write(a, "<br>")
+// ---------------------
+// let b = 5;
+// document.write(b, "<br>")
+// ---------------------
+
+// let sum = a + b;        // 15
+// document.write(`Sum of ${a} and ${b} is ${sum} <br>`)
+// ---------------------
+
+// let difference = a - b; // 5
+// document.write(`Differ of ${a} and ${b} is ${difference} <br>`)
+// ---------------------
+
+
+// let product = a * b;    // 50
+// ---------------------
+// let quotient = a / b;   // 2
+// ---------------------
+// let remainder = a % b;  // 0
+// ---------------------
+
+// let isEqual = (a == b) ? "Equal" : "Not Equal"; // false
+// document.write(`Values ${a} and ${b} is ${isEqual}`)
+// ---------------------
+// let a = 10
+// let b = 12
+// let isEqual = !(a == b) ? "Not Equal" : "Equal"; // false
+// document.write(`Values ${a} and ${b} is ${isEqual}`)
+
+// ---------------------
+
+// let a = 10
+// let b = 12
+// let isNotEqual = (a != b) ? "Not Equal" : "Equal"; // false
+// document.write(`Values ${a} and ${b} is ${isNotEqual}`)
+
+// ----------------------
+// let a = prompt("Enter first value")
+// let b = prompt("Enter Second value")
+// let isGreater = (a > b) ? `${a} is greater`: `${b} is greater`;   // true
+// document.write(isGreater)
+
+
+// --------------------
+// let a = 100
+// let b = 20
+// let logicalAnd = (a > b && b < 10); // true
+// console.log(logicalAnd);
+
+
+// --------------------
